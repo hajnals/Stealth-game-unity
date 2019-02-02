ec497862292ddfaf33a4dd26df8c66cce81565ce
Hajnal Sandor <38861030+hajnals@users.noreply.github.com>
2019-02-02 17:10:43 +0100

    Uploaded old project
    A unity game, stealth, from youtube

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed = 6;
+    public float smoothMoveTime = 0.1f;
+    public float turnSpeed = 8;
+
+    float angle;
+    float smoothInputMagnitude;
+    float smoothMoveVelocity;
+    Vector3 velocity;
+
+    Rigidbody rigidbody;
+    bool disabled;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        Guard.OnGuardHasSpottedPlayer += Disable;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 inputDirection = Vector3.zero;
+        if (!disabled) {
+            inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        }
+        //Direction
+        float inputMagnitude = inputDirection.magnitude;
+        smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+
+        float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+        angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * turnSpeed * inputMagnitude);
+
+        velocity = transform.forward * moveSpeed * smoothInputMagnitude;
+	}
+
+    void Disable()
+    {
+        disabled = true;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MoveRotation(Quaternion.Euler(Vector3.up * angle));
+        rigidbody.MovePosition(rigidbody.position + velocity * Time.deltaTime);
+    }
+
+    private void OnDestroy()
+    {
+        Guard.OnGuardHasSpottedPlayer -= Disable;
+    }
+}
